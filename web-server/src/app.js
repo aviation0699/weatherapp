02f23906ad2147f21c3ffb97a92681af8333b8b4
akhil <akhilgupta0698@gmail.com>
2021-06-16 01:45:38 +0530

    Update app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -29,7 +29,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Akhil'
     })
 })
 
@@ -37,7 +37,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Akhil'
     })
 })
 
@@ -83,7 +83,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Akhil',
         errorMessage: 'Help article not found.'
     })
 })
@@ -91,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Akhil',
         errorMessage: 'Page not found.'
     })
 })
